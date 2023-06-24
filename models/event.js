f9bff7d5e4715154286d3ed36c6fcc44c135b70f
NoahKKA <noahhhhh.a@gmail.com>
2023-06-24 10:52:50 -0700

    added stages controller

--- models/event.js
+++ models/event.js
@@ -9,8 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate(stage, stage_events) {
+      Event.belongsToMany(stage, {
+        foreignKey: 'event_id',
+        as: 'stages',
+        through: stage_events
+      })
     }
   }
   event.init({
